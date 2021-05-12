41c6c549ca58711d14a6c87057967025e7e71d26
Tulupov <>
2021-05-12 12:43:04 +0000

    Добавил краш функцию

--- main.py
+++ main.py
@@ -70,7 +70,14 @@ async def on_message(message):
 
     if message.content.startswith(':Пока'):
       await message.channel.send('Пока!')
-    
+
+
+    if message.content.startswith(':Turn on krash')
+      await message.channel.send('ОК')
+      await message.channel.send('Краш режим запущен!')
+
+
+
 
 
 client.run(os.environ['TOKEN'])
