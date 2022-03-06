ad2def93353adb25c538c00288251204b1d24e58
Tulupov <76579531+Talk-Bot@users.noreply.github.com>
2022-03-06 17:07:06 +0000

    змх

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ async def on_message(message):
       await message.channel.send(':Поддержка')
 
     if message.content.startswith(':Бен'):
-      await 🎾Комната 1🐖.connect()
+      await voicechannel.connect()
     
 
   
