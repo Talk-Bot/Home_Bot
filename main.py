7f884e74bc4fd4e4930801c2953b4f464e5016b0
Tulupov <76579531+Talk-Bot@users.noreply.github.com>
2022-03-06 18:37:28 +0000

    ,ty

--- main.py
+++ main.py
@@ -60,13 +60,13 @@ async def on_message(message):
       hm_number = 3
       haha_number = 4
       current_number = randint(1, 4)
-      if current_number == yes_number:
-        await message.channel.send('Да')
-      if current_number == no_number:
-        await message.channel.send('Нет')
-      if current_number == hm_number:
-        await message.channel.send('Хм')       if current_number == haha_number:
-        await message.channel.send('Хахаха')
+        if current_number == yes_number:
+          await message.channel.send('Да')
+        if current_number == no_number:
+          await message.channel.send('Нет')
+        if current_number == hm_number:
+          await message.channel.send('Хм')         if current_number == haha_number:
+          await message.channel.send('Хахаха')
 
 
      if message.content.startswith(':Поддержка'):
